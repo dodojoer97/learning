efc4b8b32ad5489d0fc675c2d434a37292105fb9
Ido <ido.sarue@travelfactory.fr>
2024-07-17 14:18:32 +0300

    continue

--- udemy-understanding-typescript/generics-01-starting-setup/src/app.ts
+++ udemy-understanding-typescript/generics-01-starting-setup/src/app.ts
@@ -1,22 +1,84 @@
+
 // // Code goes here!
-// const names: Array<string> = [];
-// // const names2: string[] = []
-// // names[0].split(" ");
+// const names: Array<string> = ["Max", "Manuel"]
 
 // const promise: Promise<string> = new Promise((resolve) => {
-// 	setTimeout(() => {
-// 		resolve("this is doen");
-// 	}, 2000);
-// });
+//     setTimeout(() => {
+//         resolve("done")
+//     }, 2000)
+// })
 
-// //
-// promise.then((value) => console.log(value));
+// promise.then(data => {
+//     data.split("")
+// })
 
-// Creating a generic function
-function merge<T, U>(objA: T, objB: U) {
-	// return {...objA, ...objB}
-	return Object.assign(objA, objB);
+// // Generic function
+// function merge<T extends object, U extends object>(objA: T, objB: U) {
+//     return Object.assign(objA, objB)
+// }
+
+// const mergedObj = merge({name: "max"}, {age: 30})
+
+// interface Lengthy {
+//     length: number
+// }
+
+// function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+//     let descriptionText = "Got no value"
+//     if(element.length  === 1) {
+//         descriptionText = 'Got 1 element'
+//     }else if(element.length > 1) {
+//        descriptionText = 'Got ' + element.length + ' elements'
+//     }
+ 
+    
+//     return [element, descriptionText]
+// }
+
+// console.log(countAndDescribe('hi there'))
+
+// // Keyof
+// function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+//     return obj[key]
+// }
+
+// extractAndConvert({name: "max"}, "name")
+
+// // Generic classes
+// class DataStorage<T> {
+//     private data: T[] = []
+
+//     addItem(item: T) {
+//         this.data.push(item)
+//     }
+
+//     removeItem(item: T) {
+//         this.data.splice(this.data.indexOf(item), 1)
+//     }
+
+//     getItems() {
+//         return [...this.data]
+//     }
+// }
+
+// const textStorage = new DataStorage<string>()
+// textStorage.addItem('test')
+
+
+// Generic utility types
+interface CourseGoal {
+    title: string
+    description: string
+    completeUntil: Date
 }
 
-const mergeOBJ = merge({ name: "ido" }, { age: 123 });
-mergeOBJ.age;
+function createCourseGoal(title: string, description: string, completeUntil: Date): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {}
+    courseGoal.title = title
+    courseGoal.description = description
+    courseGoal.completeUntil = completeUntil
+
+    return courseGoal as CourseGoal
+}
+
+const names: Readonly<string[]> = ["max", "sports"]
