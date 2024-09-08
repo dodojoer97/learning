30ba3aefad58b465319ab2c28a025eb55191bc1e
Ido <ido.sarue@travelfactory.fr>
2024-09-08 13:40:44 +0300

    add projectList class

--- udemy-understanding-typescript/section9/prj-00-initial-starting-setup/src/app.ts
+++ udemy-understanding-typescript/section9/prj-00-initial-starting-setup/src/app.ts
@@ -51,6 +51,39 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     return adjDescriptor
 }
 
+
+
+// List
+class ProjectList {
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    element: HTMLElement
+    constructor(private type: "active" | "finished") {
+        this.templateElement = document.querySelector("#project-list") as HTMLTemplateElement
+        this.hostElement = document.querySelector("#app") as HTMLDivElement
+
+        // Get the content of a html template
+        const importedNode: DocumentFragment = document.importNode(this.templateElement.content, true)
+
+        this.element = importedNode.firstElementChild as HTMLElement
+        this.element.id = `${this.type}-projects`;
+
+        this.attach()
+        this.renderContent()
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`
+        this.element.querySelector('ul')!.id = listId
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + " PROJECTS"
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element)
+    }
+}
+
+
 // ProjectInput class
 class ProjectInput {
     templateElement: HTMLTemplateElement
@@ -128,6 +161,7 @@ class ProjectInput {
         if(Array.isArray(userInput)) {
             const [title, description, people] = userInput
             console.log(title, description, people)
+            this.clearInputs()
         }
         
     }
@@ -142,4 +176,7 @@ class ProjectInput {
     }
 }
 
+
 const projectInput = new ProjectInput()
+const activeProjectList = new ProjectList("active")
+const finishedProjectList = new ProjectList("finished")
